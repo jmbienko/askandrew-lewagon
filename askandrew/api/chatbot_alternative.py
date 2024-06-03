076e495a2130a647c57c8f7d6ae095b23e2de034
jmbienko <62215489+jmbienko@users.noreply.github.com>
2024-06-03 16:29:21 +0200

    Update chatbot_alternative.py

--- askandrew/api/chatbot_alternative.py
+++ askandrew/api/chatbot_alternative.py
@@ -2,8 +2,6 @@ from askandrew.ml_logic.init_setup import database_loading
 from askandrew.ml_logic.llm_setup import return_llm, start_llm
 from askandrew.ml_logic.question_answer import user_input, answer_mmr, llm_based_answer
 
-#from askandrew.ml_logic import database_loading, return_llm, user_input, answer_mmr, llm_based_answer
-
 def main():
     # Call the user_input function to get the question from the user.
     question = user_input()
