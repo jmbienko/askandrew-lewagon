eabf830b699ac420243226d26282a35601e24f38
jmbienko <62215489+jmbienko@users.noreply.github.com>
2024-06-03 16:27:52 +0200

    Update init_setup.py

--- askandrew/ml_logic/init_setup.py
+++ askandrew/ml_logic/init_setup.py
@@ -1,27 +1,11 @@
 from langchain_community.vectorstores import Chroma
-#from langchain_community import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
-#from langchain.vectorstores import Chroma
 import os
-#from params.params import openai_api_key
 import openai
 from askandrew.params import persist_directory
 
 def database_loading():
     embedding = OpenAIEmbeddings()
-    #openai.api_key = openai_api_key
-
     openai.api_key = os.environ.get('OPENAI_API_KEY')
-
-    ###!!!!!remember to change it to absolute path!!!
-    #persist_directory = "/home/martina/code/jmbienko/askandrew-lewagon/askandrew/chroma"
-
     vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
-    #vectordb.get()
- #   print(f"succesfully loaded vectordb with {vectordb._collection.count()} embeddings")
-
     return vectordb
-
-
-    #sample_items = vectordb._collection.count()
-    #print(sample_items)
