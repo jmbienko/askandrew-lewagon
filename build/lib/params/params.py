b76323764d4beaf4a69bc6f56d291f4a88b36ffe
jmbienko <62215489+jmbienko@users.noreply.github.com>
2024-06-03 16:24:16 +0200

    Update params.py

--- build/lib/params/params.py
+++ build/lib/params/params.py
@@ -1,20 +1,10 @@
-
 import os
 
 
 open_api_key = os.environ.get('OPENAI_API_KEY')
 
-
-####!!!!!!!!!remember to change to relative path later!!!!!!!!!!!!!!
 local_directory = "/Users/joanna/Documents/GitHub/askandrew-lewagon/"
 
-
-### please note that we have not included any params from the data-processing stage
-### only included as of "ml_logic"
+persist_directory = local_directory +'/askandrew/chroma/' 
 
 
-persist_directory = local_directory +'/askandrew/chroma/' #setting the location of the local directory
-
-### ML_LOGIC
-
-#
